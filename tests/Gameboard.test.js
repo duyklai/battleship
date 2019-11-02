08371ca689bb10bded8b6b0af4e8b58b64397224
Duy Lai <duyklai@gmail.com>
2019-11-02 00:32:07 -0500

    Update old tests to match param and add new sukenAll() test

--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -1,34 +1,46 @@
 const Board = require('../src/Gameboard');
+const Ship = require('../src/Ship');
 
-describe('testing gameboard', () => {
+describe('Testing placeShip', () => {
   let board = null;
   beforeEach(() => {
     board = Board();
   });
 
-  // Serializes to the same string error
-  // test.only('Ship placed correctly', () => {
+  //Serializes to the same string error
+  // test('Ship placed correctly', () => {
   //   const carrier = Ship(
   //     'Carrier',
   //     5,
   //     { x_coor: 3, y_coor: 3 },
   //     { x_coor: 3, y_coor: 7 }
   //   );
-  //   let board = [...Array(10)].map(e => Array(10));
+  //   let created_board = [...Array(10)].map(e => Array(10));
   //   count = 0;
   //   while (count < 5) {
-  //     board[3][3 + count] = carrier;
+  //     created_board[3][3 + count] = carrier;
   //     count++;
   //   }
-  //   expect(game.placeShip(3, 3, 'Carrier')).toEqual(board);
+  //   // console.log(board.placeShip(3, 3, 'Carrier', 'horizontal'));
+  //   // console.log(created_board);
+  //   expect(board.placeShip(3, 3, 'Carrier', 'horizontal')).yourMatcher(
+  //     created_board
+  //   );
   // });
 
   test('Return undefined when placing ship will cause out of bounds for x', () => {
-    expect(board.placeShip(7, 2, 'Carrier', 'horizontal')).toBeUndefined();
+    expect(board.placeShip(7, 2, 'Carrier', 'vertical')).toBeUndefined();
   });
 
   test('Return undefined when placing y will cause outOfBounds', () => {
-    expect(board.placeShip(4, 8, 'Carrier', 'vertical')).toBeUndefined();
+    expect(board.placeShip(4, 8, 'Carrier', 'horizontal')).toBeUndefined();
+  });
+});
+
+describe('Testing receiveAttack', () => {
+  let board = null;
+  beforeEach(() => {
+    board = Board();
   });
 
   test('Returns true when ship has been hit', () => {
@@ -51,5 +63,27 @@ describe('testing gameboard', () => {
     board.placeShip(2, 2, 'Carrier', 'horizontal');
     board.receiveAttack(2, 3);
     expect(board.receiveAttack(2, 3)).toBeNull();
+  });
+});
+
+describe('Testing sunkenAll', () => {
+  let board = null;
+  beforeEach(() => {
+    board = Board();
+  });
+
+  test('Returns true when all ships(1) has been sunk', () => {
+    board.placeShip(2, 2, 'Patrol', 'horizontal');
+    board.receiveAttack(2, 2);
+    board.receiveAttack(2, 3);
+    expect(board.sunkenAll()).toBeTruthy();
+  });
+
+  test('Returns false when only 1 ship (out of 2) has been sunk', () => {
+    board.placeShip(2, 2, 'Patrol', 'horizontal');
+    board.placeShip(4, 4, 'Submarine', 'horizontal');
+    board.receiveAttack(2, 2);
+    board.receiveAttack(2, 3);
+    expect(board.sunkenAll()).toBeFalsy();
   });
 });
